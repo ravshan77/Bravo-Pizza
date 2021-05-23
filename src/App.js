2a5ecbc9f22476012d914d1a6991bc646b2f4117
Johnny <jakhongir12@users.noreply.github.com>
2021-05-23 14:25:43 +0500

    Jakhongir's commit

--- src/App.js
+++ src/App.js
@@ -1,88 +1,70 @@
-import "./App.css";
-import { Route, Switch } from "react-router-dom";
-import FilteredByCost from "./pages/filtered/filteredByCost"
-import React,{ lazy, Suspense } from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import FilteredByCost from './pages/filtered/filteredByCost';
+import React, { lazy, Suspense } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Pizza from "./pages/pizza/pizza";
-import Drink from "./pages/drink/drink";
-import Food from "./pages/food/food";
-import Cake from "./pages/cake/cake";
-import { useSelector } from "react-redux"
-import NewHeader from "./components/newHeader/NewHeader";
-import BottomHeader from "./components/Bottom-Header/bottomHeader"
-import ProductModal from './components/productModal/ProductModal'
+import Pizza from './pages/pizza/pizza';
+import Drink from './pages/drink/drink';
+import Food from './pages/food/food';
+import Cake from './pages/cake/cake';
+import { useSelector } from 'react-redux';
+import NewHeader from './components/newHeader/NewHeader';
+import BottomHeader from './components/Bottom-Header/bottomHeader';
+import ProductModal from './components/productModal/ProductModal';
+import Caarousel from './components/carousel/Carousel';
 
 const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
+	backdrop: {
+		zIndex: theme.zIndex.drawer + 1,
+		color: '#fff'
+	}
 }));
 
-
-
 function App() {
-  
-  const {foods} = useSelector((state) => state.cardData);
+	const { foods } = useSelector((state) => state.cardData);
 
-  
-  console.log("stordan App ga kegan foods",foods)
-  
-  const classes = useStyles();
+	console.log('stordan App ga kegan foods', foods);
 
+	const classes = useStyles();
 
+	return (
+		<div className='App'>
+			<NewHeader />
+			<BottomHeader />
+			<Caarousel />
+			<ProductModal />
+			<Switch>
+				<Route path='/' exact render={(props) => <Pizza props={props} />} />
 
+				<Route path='/drink' exact render={(props) => <Drink props={props} />} />
 
-  return (
-    <div className="App">
-      <NewHeader />
-      <BottomHeader />
-      <ProductModal />
-      <Switch>
-        <Route 
-          path="/" 
-          exact 
-          render={(props) => <Pizza props={props} />} 
-          />
+				<Route path='/food' exact render={(props) => <Food props={props} />} />
 
-        <Route 
-          path="/drink" 
-          exact 
-          render={(props) => <Drink props={props} />} />
+				<Route path='/cake' exact render={(props) => <Cake props={props} />} />
 
-        <Route 
-          path="/food" 
-          exact 
-          render={(props) => <Food props={props} />} />
-
-        <Route 
-          path="/cake" 
-          exact 
-          render={(props) => <Cake props={props}  />} />
-
-        <Route
-        path="/filteredByCost/:id"
-        render={(props) => {
-          return <FilteredByCost props={props} data={foods} />;
-        }}
-        />  
-      </Switch>
-      {/* <ThemeComponenet /> */}
-      {/* <CallBack /> */}
-      {/* <ThemeProvider>
+				<Route
+					path='/filteredByCost/:id'
+					render={(props) => {
+						return <FilteredByCost props={props} data={foods} />;
+					}}
+				/>
+			</Switch>
+			{/* <ThemeComponenet /> */}
+			{/* <CallBack /> */}
+			{/* <ThemeProvider>
         <ChildComponent />
         <ParentComponent />
         <ToggleBtn />
       </ThemeProvider> */}
-      {/* <Memo /> */}
-      {/* <Suspense fallback={<h1>loading...</h1>} >
+			{/* <Memo /> */}
+			{/* <Suspense fallback={<h1>loading...</h1>} >
         <Card />
         <List />
       </Suspense> */}
-      {/* <Footer/> */}
-      {/* <Example/> */}
-    </div>
-  );
+			{/* <Footer/> */}
+			{/* <Example/> */}
+		</div>
+	);
 }
 
 export default App;
