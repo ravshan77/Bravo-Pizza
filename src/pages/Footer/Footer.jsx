4f044fe318c9538bd0f4e6e8b745a35d82ff80b0
gulhayoarabova <gulhayoarabova@gmail.com>
2021-06-01 20:50:58 +0500

    “corrected3”

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -32,9 +32,7 @@ const Footer = () => {
         <div className="div-one">
           <h4>FOLLOW US</h4>
           <p>Quaerat neque purus ipsum at neque dolor primis tempus</p>
-          <p>Quaerat neque purus ipsum at neque dolor primis tempus purus  </p>
-          <p>Lorem ipsum </p>
-          <p>Quaerat neque purus ipsum at neque dolor primis tempus</p>
+          <p>Quaerat neque purus ipsum at neque  </p>
         </div>
       </div>
         <div className="icon-links">
